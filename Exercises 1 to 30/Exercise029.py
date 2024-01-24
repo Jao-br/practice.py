dab0311fdafa97509cddb0907f3f7ef00414e6fc
João Carlos Castro <123jccml@gmail.com>
2024-01-24 17:51:35 -0300

    :sparkles: Add Random Stuff

--- Exercises 1 to 30/Exercise029.py
+++ Exercises 1 to 30/Exercise029.py
@@ -1,12 +1,10 @@
 # Write a program that reads the speed of a car. If he exceeds 80 km/h, show a message saying that he has been fined. The fine will cost R$7.00 for each km over the limit.
 
 speed = int(input("Input the guaded speed: "))
-overLimit = 
 fee = 0 
 
 if speed > 80: 
     fee += (speed - 80) * 7
-    print(f"You car was over the speed limit ")
+    print(f"Your car was {speed - 80} km/h over the speed limit. The fine is R${fee}")
 else:
-        
-        
+    print(f"Your car was not over the speed limit.")
