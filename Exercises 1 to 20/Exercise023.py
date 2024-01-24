787f9e9476f28f6dde68d8b616c594b3a651071d
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-24 09:58:59 -0300

    :sparkles: Solve Exercise023.py

---
+++ Exercises 1 to 20/Exercise023.py
@@ -0,0 +1,7 @@
+# Write a program that reads a number from 0 to 9999 and displays each of the separate digits on the screen.
+
+num = str(input("Input number: "))
+SplitedNum = num.replace("", " ")
+
+print(SplitedNum)
+
