85dcfc6528f5f7f9f629e61faaa29eb30c7722f6
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-24 10:44:22 -0300

    :bug: Refactor Exercise024.py

--- Exercises 1 to 30/Exercise024.py
+++ Exercises 1 to 30/Exercise024.py
@@ -1,8 +1,6 @@
-# Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+CityName = input("Input City Name: ").lower()
 
-CityName = input("Input City Name: ")
-
-if "joão"or"lucas"or"mateus"or"santos"or"santo"or"são" in CityName.lower():
-    print("This Brazilian city does have a religious name.") 
-else: 
+if any(x in CityName for x in ["joão", "lucas", "mateus", "santos", "santo", "são"]):
+    print("This Brazilian city does have a religious name.")
+else:
     print("This Brazilian city does NOT have a religious name.")
