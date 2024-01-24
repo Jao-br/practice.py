267cac1fc12466155d2cebce2e7ab7dd5a4d9c3b
João Carlos Castro <123jccml@gmail.com>
2024-01-24 20:05:22 -0300

    :sparkles: Solve Exercise035.py

--- Exercises 1 to 30/Exercise035.py
+++ Exercises 1 to 30/Exercise035.py
@@ -1,2 +1,10 @@
 # Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 
+a = float(input("Input the first side of the triangle: "))
+b = float(input("Input the second side of the triangle: "))
+c = float(input("Input the third side of the triangle: "))
+
+if (a < b + c) and (b < a + c) and (c < a + b):
+    print("The values you typed in CAN form a triangle!")
+else:
+    print("The values you typed in CAN´T form a triangle")
