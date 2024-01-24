93f427bb21b152e54bca8e478f2101b7adc64461
João Carlos Castro <123jccml@gmail.com>
2024-01-24 19:32:48 -0300

    :tada: Solve Exercise033.py

--- Exercises 1 to 30/Exercise033.py
+++ Exercises 1 to 30/Exercise033.py
@@ -1,2 +1,9 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 
+numbers = []
+n = str(input("Input 3 diferent numbers separated by spaces: "))
+
+for i in n.split():
+    numbers.append(int(i))
+
+print(f"The largest number you typed in is {max(numbers)} and the smallest is {min(numbers)}")
