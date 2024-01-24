d6a8eea8c0d45420a45353aca321951065c51f05
João Carlos Castro <123jccml@gmail.com>
2024-01-24 18:53:14 -0300

    :sparkles: Solve Exercise031.py

--- Exercises 1 to 30/Exercise031.py
+++ Exercises 1 to 30/Exercise031.py
@@ -1,2 +1,7 @@
 # Develop a program that asks the distance of a trip in km. Calculate the price of the ticket, charging R$0.50 per km for trips of up to 200 km and R$0.45 for longer trips.
 
+distance = int(input("Input the distance in KM: "))
+if distance <= 200:
+    price = distance * 0.50
+else:
+    price = distance * 0.45
