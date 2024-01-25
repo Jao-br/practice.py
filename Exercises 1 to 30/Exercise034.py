f23a5eb3d0ef2a38d7518ed2911e8e5f3aaccb0d
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-25 10:13:49 -0300

    :sparkles: Solve Exercise042.py

--- Exercises 1 to 30/Exercise034.py
+++ Exercises 1 to 30/Exercise034.py
@@ -3,6 +3,6 @@
 salary = float(input("Input salary: "))
 
 if salary > 1250.00:
-    print(f"The previous salary was {salary:.2f}\n - With a 10% raise the new compensation is R${salary * 1.10:.2f}")
+    print(f"The previous salary was R${salary:.2f}\n - With a 10% raise the new compensation is R${salary * 1.10:.2f}")
 else:
-    print(f"he previous salary was {salary:.2f}\n - With a 15% raise the new compensation is R${salary * 1.15:.2f}")
+    print(f"he previous salary was R${salary:.2f}\n - With a 15% raise the new compensation is R${salary * 1.15:.2f}")
--- Exercises 1 to 30/Exercise042.py
+++ Exercises 1 to 30/Exercise042.py
@@ -2,3 +2,16 @@
 # - EQUILÁTERO: todos os lados iguais
 # - ISÓSCELES: dois lados iguais, um diferente
 # - ESCALENO: todos os lados diferentes
+
+a = float(input("Input the first side of the triangle: "))
+b = float(input("Input the second side of the triangle: "))
+c = float(input("Input the third side of the triangle: "))
+
+if (a < b + c) and (b < a + c) and (c < a + b):
+    print("The values you typed in CAN form a triangle!")
+elif a == b == c:
+    print("O triangulo formado é Equilatero")
+elif (a == b != c) or (a == c != b) or (b == c != a):
+    print("O triangulo formado é Isóceles")
+elif a != b !=c: 
+    print("O triangulo formado é ESCALENO")
