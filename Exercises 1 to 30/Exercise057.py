eaef62978cad9f6a3de5a12966e85a61626599f1
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-26 09:30:23 -0300

    :sparkles: Solve Exercise057.py

---
+++ Exercises 1 to 30/Exercise057.py
@@ -0,0 +1,9 @@
+#  Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
+letra = str(input("Insira valores F ou M: ")).upper()
+
+while letra not in "FM":
+    print("Valor incorreto, Digite novamente!")
+    break
+
+print("valor correto!, obrigado.")
