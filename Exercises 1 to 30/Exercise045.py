2687fec26d637ee4fa34a97e8bdc81fe9cd0aea1
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-30 11:54:09 -0300

    :boom: Makes Exercise045.py Funnier

--- Exercises 1 to 30/Exercise045.py
+++ Exercises 1 to 30/Exercise045.py
@@ -9,8 +9,16 @@ print('---------- JOKENPO GAME: ----------')
 print('Select:\n[1] for ROCK,\n[2] for SCISSORS or\n[3] for PAPER.')
 playerinput = int(input('Chose your fighter: '))
 
-sleep(1)
-print(f'-' * 30)
+sleep (0.5)
+
+if playerinput == 1:
+    print(f'You chose: ROCK')
+elif playerinput == 2:
+    print(f'You chose: SCISSORS')
+elif playerinput == 3:
+    print(f'You chose: PAPER')
+
+sleep(0.5)
 
 if computer == 1:
     print(f'The computer chose: ROCK')
@@ -18,7 +26,9 @@ elif computer == 2:
     print(f'The computer chose: SCISSORS')
 elif computer == 3:
     print(f'The computer chose: PAPER')
+print(f'-' * 15, 'RESULT','-' * 15)
 
+sleep(1)
 
 if computer == 1:
     if playerinput == 1:
@@ -43,6 +53,3 @@ elif computer == 3:
         print(f'VICTORY!! SCISSORS beats PAPER')
     elif playerinput == 3:
         print(f'TIE! You chose the same.')
-
-
-print('THATS THE END OF ALL')
