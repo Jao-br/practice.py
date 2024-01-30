45b1bbba3e83aa28efa04a6d2eae0229eab6c047
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-30 10:36:59 -0300

    :recycle: Refactor Exercise065.py

--- Exercises 1 to 30/Exercise065.py
+++ Exercises 1 to 30/Exercise065.py
@@ -9,7 +9,8 @@ print("To finalize the inputs and perform te claculations, input -1")
 
 while num != -1:
     num = int(input("Input an Integer number: "))
-    numlist.append(num)
-    numcount += 1
+    if num !=  -1:
+        numlist.append(num)
+        numcount += 1
 
 print(f"You typed a total of {numcount} numbers.\nThe largest one being {max(numlist)} and the smallest {min(numlist)}.\nFurthermore, the avarage of the numbers you typed is {(sum(numlist)/numcount)}")
