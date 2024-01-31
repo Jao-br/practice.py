3acfd1aa44bed859982a7d89e79b4b5fe8c4c6b2
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-31 08:46:07 -0300

    :Sparkles: Solve Exercise046.py

--- Exercises 1 to 30/Exercise046.py
+++ Exercises 1 to 30/Exercise046.py
@@ -1,1 +1,9 @@
 # Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+from time import sleep
+
+print('FIREWORK COUNTDOWN:')
+num = 10 
+for i in range(num, -1, -1):
+    sleep(1)
+    print(f'{i} seconds missing')
+    
