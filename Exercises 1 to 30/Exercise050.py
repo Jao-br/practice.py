081e884fe1f1ac8a12103381a282118f4e4b2af6
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-31 11:26:31 -0300

    :sparkles: Solve Exercise050.py

--- Exercises 1 to 30/Exercise050.py
+++ Exercises 1 to 30/Exercise050.py
@@ -1,2 +1,14 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
+evenNumList = []
+oddNumList = []
+
+for i in range(0,6):
+    num = int(input('Type an even number (Odd numbers will be disconsidered): '))
+    if num % 2 == 0:
+        evenNumList.append(num)
+    else:
+        oddNumList.append(num)
+
+print(f'The sum of the even numbers {sum(evenNumList)}')
+print(f'The Odd numbers you mistakenly typed are {oddNumList}')
