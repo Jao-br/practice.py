55b5f07b54c36af50b98f2b1404ed450333dd8c2
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-31 10:52:58 -0300

    :sparkles: Solve Exercise047.py

--- Exercises 1 to 30/Exercise047.py
+++ Exercises 1 to 30/Exercise047.py
@@ -1,2 +1,8 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
+numbers = []
+for i in range(1,51):
+    print(i)
+    numbers.append(i)
+
+print(numbers)
