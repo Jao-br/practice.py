9774bc4f1b383cb02a5af39a1c202167d2f96825
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-31 10:09:29 -0300

    :sparkles: Solve Exercise043.py

--- Exercises 1 to 30/Exercise043.py
+++ Exercises 1 to 30/Exercise043.py
@@ -4,3 +4,23 @@
 # - 25 até 30: Sobrepeso
 # - 30 até 40: Obesidade
 # - Acima de 40: Obesidade Mórbida
+
+weight = float(input('Input your weight in Kilos(kg): '))
+height = float(input('Input your height in Meters(m): '))
+bmi = weight / (height ** 2)
+
+if bmi < 18.5:
+    print(f'Your Body Mass Index is of {bmi:.2f}, with is {(18.5 - bmi):.2f} points below the ideal Index.')
+    print('> This BMI classifies as Below Ideal.')
+elif bmi < 25: 
+    print(f'Your Body Mass Index is of {bmi:.2f}, wich is at a normal weight.')
+    print('> This BMI classifies as Ideal.')
+elif bmi < 30:
+    print(f'Your Body Mass Index is of {bmi:.2f}, with is {-1*(25 - bmi):.2f} points above the ideal Index.')
+    print('> This BMI classifies as Overweight.')
+elif bmi <= 40:
+    print(f'Your Body Mass Index is of {bmi:.2f}, with it {-1*(25 - bmi):.2f} points above the ideal Index.')
+    print('> This BMI classifies as Obesity.')
+else:
+    print(f'Your Body Mass Index is of {bmi:.2f}. with it {-1*(25 - bmi):.2f} points above the ideal Index.')
+    print('> This BMI classifies Morbidity Obesity.')
