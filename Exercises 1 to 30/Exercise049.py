02ea41d13a0cec745e4897b9a9c4789f303edb16
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-31 11:09:47 -0300

    :sparkles: Solve Exercise049.py

--- Exercises 1 to 30/Exercise049.py
+++ Exercises 1 to 30/Exercise049.py
@@ -1,2 +1,9 @@
 # Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
 
+num = int(input("Insira um número para calcular sua tabuada: "))
+value = 0
+
+for i in range(0, 11):
+    print(f"{num} X {value} = {num * value}")
+    value += 1
+
--- Exercises 1 to 30/Exercise0490.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Digite um númeor para fazermos a tabuada dele: "))
-value = 0
-for i in range(0, 11):
-   
-    print(f"{num} X {value} = {num * value} 😎")
-    value += 1
-
