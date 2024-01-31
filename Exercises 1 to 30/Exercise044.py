ab1e1d66652fa796f49ab2f1659575898298d4f7
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-01-31 09:09:37 -0300

    :Sparkles: Solve Exercise044.py

--- Exercises 1 to 30/Exercise044.py
+++ Exercises 1 to 30/Exercise044.py
@@ -3,3 +3,22 @@
 # - à vista no cartão: 5% de desconto
 # - em até 2x no cartão: preço formal
 # - 3x ou mais no cartão: 20% de juros
+
+price = float(input('Input produt price: R$'))
+
+print(f'Digite o número da opção de pagamento desejada:\n [1] À vista dinheiro/cheque:  10% Off.\n [2] À vista no cartão:  5% Off.\n [3] Em até 2x no cartão:  Preço Formal\n [4] 3x ou mais no cartão:  20% de Juros sob o Montante Total')
+pay_method = int(input('Opção: '))
+
+if pay_method == 1:
+    price = price * 0.9
+elif pay_method == 2:
+    price = price * 0.95
+elif pay_method == 3:
+    price = price 
+elif pay_method == 4:
+   price = price * 1.2
+else: 
+    print("Invalid payment method")
+
+print(f'The Fina Price is: R${price}')
+
