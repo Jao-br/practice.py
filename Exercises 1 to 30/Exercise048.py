09779ff9540da92bdc96931e81bc42e7dacce606
JOÃO CARLOS CASTRO MENDES DE LIMA <123jccml@gmail.com>
2024-02-01 10:58:03 -0300

    :sparkles: Solve exercise048.py

--- Exercises 1 to 30/Exercise048.py
+++ Exercises 1 to 30/Exercise048.py
@@ -1,1 +1,10 @@
-# Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+# Faça um programa que calcule a soma entre todos ímpares os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+ListNum = []
+for i in range(0,500):
+    if i % 2 != 0:
+        if ( i% 3 ==0 ):
+            ListNum.append(i)
+
+print(ListNum)
+print(sum(ListNum))
