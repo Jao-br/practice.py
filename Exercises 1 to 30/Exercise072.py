e2de765e6fdb55cd51a0fafc4a5c79b080c7d618
João Carlos Castro <123jccml@gmail.com>
2024-02-06 20:12:44 -0300

    Solve Exercise072.py

--- Exercises 1 to 30/Exercise072.py
+++ Exercises 1 to 30/Exercise072.py
@@ -1,1 +1,9 @@
-# Create a program that has a pair completely filled with a count in words, from zero to twenty. Your program should read a number from the keyboard (between 0 and 20) and display it in full.
+# Create a program that has a pair completely filled with a count in words, from zero to ten. Your program should read a number from the keyboard (between 0 and 20) and display it in full.
+
+numbers = ("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+while True:
+    number = int(input("Enter a number between 0 and 10: "))
+    if 0 <= number <= 10:
+        break
+    print("Try again. ", end="")
+print(f"You entered {numbers[number]}.")
